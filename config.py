5f4256d534f7b97eaee405d3c1b9a254198803c8
deborah kaplan <deborahgu@users.noreply.github.com>
2025-11-08 21:09:02 -0500

    chore: comments
    comments in the config file

--- config.py
+++ config.py
@@ -2,6 +2,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    """Create the settings.
+    
+    Don't populate here. The variables are only declared to make life
+    easier for IDE autocomplete. Populate in .env.shared -- or, if
+    committing to source control, .env.private (which is in the
+    .gitignore).
+    
+    Source for each of these strings:
+
+    Unless otherwise specified all files are on you speaker in:
+    /var/volatile/lib/Bose/PersistenceDataRoot/BoseApp-Persistence/1
+
+    - device_id: Recents.xml
+       
+    """
     base_url: str = ''
     device_id: str = ''
     device_serial_number: str = ''
