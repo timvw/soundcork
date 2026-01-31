46333a23eff1eb40c5165e5278920a834ee0c409
Allen Petersen <allen@suberic.net>
2026-01-30 22:57:32 -0500

    ha ha fixing another typo

--- soundcork/marge.py
+++ soundcork/marge.py
@@ -375,7 +375,7 @@ def account_full_xml(account: str, datastore: "DataStore") -> ET.Element:
     ET.SubElement(account_elem, "mode").text = "global"
 
     # FIXME we can get this from the language endpoint but it returns a
-    # number rathen than a language code
+    # number rather than a language code
     ET.SubElement(account_elem, "preferredLanguage").text = "en"
     account_elem.append(provider_settings_xml(account))
     account_elem.append(
